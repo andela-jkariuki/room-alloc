20cc1aee719aed831cc27a1819bccc23914e03fa
andela-jkariuki <john.kariuki@andela.com>
2016-05-10 14:18:12 +0300

    Refactor create_room and fetch data to class methods

--- tests/test_rooms.py
+++ tests/test_rooms.py
@@ -21,18 +21,13 @@ class RoomTest(unittest.TestCase):
 
         Assert that woodwing is of type living space (L)
         """
-        all_rooms = self.db.select("SELECT * FROM rooms")
+        all_rooms = self.fetch_data("rooms", False)
         self.assertEqual(0, len(all_rooms))
 
-        rooms = Rooms("test.db")
-        arguments = {'<room_name>': ['woodwing', 'westwing', 'eastwing'],
-                     'living': True,
-                     'office': False}
-
-        new_rooms = rooms.create_rooms(arguments)
+        new_rooms = self.create_living_spaces()
         self.assertTrue(new_rooms)
 
-        all_rooms = self.db.select("SELECT * FROM rooms")
+        all_rooms = self.fetch_data("rooms", False)
         self.assertEqual(3, len(all_rooms))
         self.assertTrue('woodwing' in all_rooms[0])
         self.assertTrue('L' in all_rooms[0])
@@ -42,6 +37,21 @@ class RoomTest(unittest.TestCase):
 
         if os.path.exists('test.db'):
             os.remove('test.db')
+
+    def create_living_spaces(self):
+        rooms = Rooms('test.db')
+        arguments = {'<room_name>': ['woodwing', 'westwing', 'eastwing'],
+                     'living': True,
+                     'office': False}
+
+        return rooms.create_rooms(arguments)
+
+    def fetch_data(self, table_name, single_record=True):
+        if single_record:
+            return self.db.select_one("SELECT * FROM %s" % (table_name))
+        else:
+            return self.db.select("SELECT * FROM  %s" % (table_name))
+
 
 if __name__ == '__main__':
     unittest.main()
